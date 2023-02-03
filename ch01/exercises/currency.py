0732f63e2c1088274ad41a3f4bef02ba54500611
Michael Kim <gamingleaf01@gmail.com>
2023-02-02 21:47:09 -0500

    Completed Currency Conversion Excercise

--- ch01/exercises/currency.py
+++ ch01/exercises/currency.py
@@ -1,9 +1,9 @@
 rate = input("Enter Current Exchange Rate For Euro To Dollar:")
 rate = float(rate)
-print(rate)
+print("You have entered:", rate)
 amount = input("Enter Amount To Exchange:")
 amount = float(amount)
-print(amount)
+print("You have entered:", amount)
 total = amount*rate
 result  = total - 3.00
 print("result:",result)
