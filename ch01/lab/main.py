cbba8956daed0b7e0b97055418d25bb1bc917ac8
Michael Kim <gamingleaf01@gmail.com>
2023-02-05 18:56:18 -0500

    Completed Chapter 1 Lab

--- ch01/lab/main.py
+++ ch01/lab/main.py
@@ -1,1 +1,21 @@
+#Part A
+weeks = 16
+classes = 5
+tuition = 6000
+cost_per_week = ((tuition / classes) / weeks)
+print("Cost per week:", cost_per_week)
+classes_per_week = 3
+cost_per_class = cost_per_week/classes_per_week
+print("This is how much you spend per class:", cost_per_class)
+print(weeks, type(weeks))
+print(classes, type(classes))
+print(tuition, type(tuition))
+print(cost_per_week, type(cost_per_week))
+print(classes_per_week, type(classes_per_week))
+print(cost_per_class, type(cost_per_class))
 
+#Part B
+import random
+myrandomlist = [3, "atoms", 9.172, "steak", 5.63, 5, "molecules", 3.14, "parsely", "butter", 19, 828.927320, "salt"]
+roulette_of_mystery = random.choice(myrandomlist)
+print("Roulette of Mystery:", roulette_of_mystery)
