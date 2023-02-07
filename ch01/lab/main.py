6fba6334b431bce87f8a9ca00b49dfb75a4fe260
Michael Kim <gamingleaf01@gmail.com>
2023-02-06 19:17:46 -0500

    Completed Chapter 1 Lab

--- ch01/lab/main.py
+++ ch01/lab/main.py
@@ -5,7 +5,7 @@ tuition = 6000
 cost_per_week = ((tuition / classes) / weeks)
 print("Cost per week:", cost_per_week)
 classes_per_week = 8
-cost_per_class = cost_per_week/classes_per_week
+cost_per_class = (cost_per_week/classes_per_week)
 print("This is how much I spend per class:", cost_per_class)
 print(weeks, type(weeks))
 print(classes, type(classes))
@@ -18,4 +18,4 @@ print(cost_per_class, type(cost_per_class))
 import random
 myrandomlist = [3, "atoms", 9.172, "steak", 5.63, 5, "molecules", 3.14, "parsely", "butter", 19, 828.927320, "salt"]
 roulette_of_mystery = random.choice(myrandomlist)
-print("Roulette of Mystery:", roulette_of_mystery)
+print(roulette_of_mystery)
---
+++ ch02/exercises/myturtle.py
@@ -0,0 +1,8 @@
+import turtle
+my_turtle = turtle.Turtle()
+my_turtle.forward(10)
+my_turtle.color("green")
+my_turtle.left(90)
+my_turtle.right(10)
+window = turtle.Screen()
+window.exitonclick()
