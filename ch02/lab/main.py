b0c3564727319cdc8f8a6e871352a39a09ecea39
Michael Kim <gamingleaf01@gmail.com>
2023-02-13 11:49:35 -0500

    Working on Lab for Chapter 2 - Not Yet Completed

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,10 +1,21 @@
+#Part A:
 
-#Part A
-weeks = 16
-classes = 5
-tuition = 6000
-cost_per_week = ((tuition / classes) / weeks)
-print("Cost per week:", cost_per_week)
-
-
-#Part B
+#Race 1
+import random
+x = random.randint(1,100)
+import turtle
+window = turtle.Screen()
+ford_turtle = turtle.Turtle()
+ford_turtle.shape("turtle")
+ford_turtle.color("blue")
+ferrari_turtle = turtle.Turtle()
+ferrari_turtle.shape("turtle")
+ferrari_turtle.color("red")
+for _ in ford_turtle.forward(x), ferrari_turtle.forward(x):
+    ford_turtle.penup()
+    ford_turtle.pendown()
+    ferrari_turtle.penup()
+    ferrari_turtle.pendown()
+    ford_turtle.goto(-100,20)
+    ferrari_turtle.goto(-100,-20)
+window.exitonclick()
