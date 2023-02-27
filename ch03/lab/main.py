7bb7ccee2dc784e1bcde999771863787fd7d1b78
Michael Kim <gamingleaf01@gmail.com>
2023-02-26 21:46:17 -0500

    Working on Lab 3

--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -1,26 +1,20 @@
-import t # 4. Pick up the pen so we don’t get lines
-leonardo.up()
-michelangelo.goto(-100,20)
-leonardo.goto(-100,-20)
-
-## 5. Your PART A code goes here
-
-
-# PART B - complete part B here
-
-
-window.exitonclick()urtle #1. import modules
 import random
+import pygame
 
 #Part A
-window = turtle.Screen() # 2.  Create a screen
-window.bgcolor('lightblue')
+pygame.init()
+window = pygame.display.set_mode()
+screen_size = window.get_size()
+dimmensions1 = screen_size[0]/2, screen_size[1]/2
+x1 = screen_size[0]/2
+y1 = screen_size[1]/2
+x2 = screen_size[0]
+y2 = screen_size[1]
+window.fill("blue")
+pygame.draw.circle(window, "black", dimmensions1, y1) 
+pygame.draw.circle(window, "pink", dimmensions1, y1 - 5)
+pygame.draw.line(window, "black", (x1,0),(x1, y2), 3)
+pygame.draw.line(window, "black", (0,y1),(x2, y1), 3)
+pygame.display.flip()
+pygame.time.wait(2000)
 
-michelangelo = turtle.Turtle() # 3.  Create two turtles
-leonardo = turtle.Turtle()
-michelangelo.color('orange')
-leonardo.color('blue')
-michelangelo.shape('turtle')
-leonardo.shape('turtle')
-
-michelangelo.up()
