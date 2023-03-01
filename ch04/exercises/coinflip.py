2031c2e1006338ba7b024d80312733aeb3971c1a
Michael Kim <gamingleaf01@gmail.com>
2023-03-01 17:49:26 -0500

    Working on Coin Flip Exercise

--- ch04/exercises/coinflip.py
+++ ch04/exercises/coinflip.py
@@ -1,5 +1,15 @@
-fruit = ["banana", "apple", "cherry"]
-print(fruit)
-fruit[0] = "pear"
-fruit[-1] = "orange"
-print(fruit)
+import turtle
+import random
+
+window = turtle.Screen()
+my_turtle = turtle.Turtle()
+my_turtle.shape("turtle")
+my_turtle.color("blue")
+heads = my_turtle.left(90)
+tails = my_turtle.right(90)
+coin_flip_forward = my_turtle.forward(50)
+
+while True:
+    for _ in 
+
+window.exitonclick()
