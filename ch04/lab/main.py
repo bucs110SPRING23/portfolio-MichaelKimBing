44bce60955f10e531a996a0f082599f7a5062063
Michael Kim <gamingleaf01@gmail.com>
2023-03-06 12:42:40 -0500

    Working on Chapter 4 Lab

--- ch04/lab/main.py
+++ ch04/lab/main.py
@@ -3,7 +3,7 @@ import pygame
 import math
 
 
-#Part A
+
 pygame.init()
 window = pygame.display.set_mode((800, 800))
 screen_size = window.get_size()
@@ -14,6 +14,52 @@ width = screen_size[0]
 height = screen_size[1]
 amount_of_throws_hits_in_circle_dart_1 = 1
 amount_of_throws_hits_in_circle_dart_2 = 1
+turns = 1
+result = ""
+
+window.fill("blue") 
+hitboxes = {
+    "red": pygame.Rect(screen_size[0]/6.4, screen_size[1]/2.85, 280, 280),
+    "green":pygame.Rect(screen_size[0]/1.67, screen_size[1]/2.85, 280, 280)
+}
+main_colors = {
+    "red": (200, 0, 0),
+    "green": (0, 200, 0)
+}
+highlight_colors = {
+    "red": (255, 0, 0),
+    "green": (0, 255, 0)
+}
+font = pygame.font.Font(None, 60)
+text1 = font.render("Guess Who Wins The Dart Game!", True, "black") 
+text2 = font.render("Please Select Your Player", True, "black")
+text3 = font.render("Player 1", True, "black")
+text4 = font.render("Player 2", True, "black")
+
+done = False
+while not done:
+    window.fill("blue")
+    pygame.draw.rect(window, main_colors["green"], hitboxes["green"])
+    pygame.draw.rect(window, main_colors["red"], hitboxes["red"])
+    window.blit(text1, (80, half_of_height/3))
+    window.blit(text2, (200, half_of_height/2))
+    window.blit(text3, (screen_size[0]/4.5, screen_size[1]/2.1))
+    window.blit(text4, (screen_size[0]/1.48, screen_size[1]/2.1))
+
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            turns = turns - 1
+            if hitboxes["red"].collidepoint(event.pos):
+                pygame.draw.rect(window, highlight_colors["red"], hitboxes["red"])
+                result = "red"
+            done = True
+            if hitboxes["green"].collidepoint(event.pos):
+                pygame.draw.rect(window, highlight_colors["green"], hitboxes["green"])
+                result = "green"
+            done = True
+    pygame.display.update()
+
+
 window.fill("blue")
 pygame.draw.circle(window, "black", center_of_screen, half_of_height) 
 pygame.draw.circle(window, "pink", center_of_screen, half_of_height - 5)
@@ -44,60 +90,24 @@ for i in range(10):
     pygame.time.wait(1000)
     pygame.display.flip()
 
-window.fill("blue")
-pygame.draw.circle(window, "black", center_of_screen, half_of_height) 
-pygame.draw.circle(window, "pink", center_of_screen, half_of_height - 5)
-pygame.draw.line(window, "black", (half_of_width,0),(half_of_width, height), 3)
-pygame.draw.line(window, "black", (0,half_of_height),(width, half_of_height), 3)
+
+font = pygame.font.Font(None, 48)
 if amount_of_throws_hits_in_circle_dart_1 > amount_of_throws_hits_in_circle_dart_2:
-    font = pygame.font.Font(None, 48)
-    text = font.render("Player 1 Wins!", True, "black")
-    window.blit(text, (half_of_width, half_of_height))
-    pygame.time.wait(5000)
+    if (result == "red"):
+        text = font.render("Correct! Player 1 Wins!", True, "black")
+    else:
+        text = font.render("Incorrect! Player 1 Wins!", True, "black")
 elif amount_of_throws_hits_in_circle_dart_1 < amount_of_throws_hits_in_circle_dart_2:
-    font = pygame.font.Font(None, 48)
-    text = font.render("Player 2 Wins!", True, "black")
-    window.blit(text, (half_of_width, half_of_height))
-    pygame.time.wait(5000)
+    if (result == "green"):
+        text = font.render("Correct! Player 2 Wins!", True, "black")
+    else:
+        text = font.render("Incorrect! Player 2 Wins!", True, "black")
 else:
-    font = pygame.font.Font(None, 48)
     text = font.render("Tie Game!", True, "black")
-    window.blit(text, (half_of_width, half_of_height))
+window.blit(text, (half_of_width, half_of_height))
+
 pygame.display.flip()
 pygame.time.wait(5000)
 
 
-#Part B:
-window.fill("blue")
-pygame.draw.rect(window, "black", [screen_size[0]/7, screen_size[1]/3, 300, 300]) 
-pygame.draw.rect(window, "red", [screen_size[0]/6.4, screen_size[1]/2.85, 280, 280])
-pygame.draw.rect(window, "black", [screen_size[0]/1.7, screen_size[1]/3, 300, 300]) 
-pygame.draw.rect(window, "green", [screen_size[0]/1.67, screen_size[1]/2.85, 280, 280]) 
-font = pygame.font.Font(None, 60)
-text1 = font.render("Guess Who Wins The Dart Game!", True, "black")
-text2 = font.render("Please Select Your Player", True, "black")
-text3 = font.render("Player 1", True, "black")
-text4 = font.render("Player 2", True, "black")
-window.blit(text1, (80, half_of_height/3))
-window.blit(text2, (200, half_of_height/2))
-window.blit(text3, (screen_size[0]/4.5, screen_size[1]/2.1))
-window.blit(text4, (screen_size[0]/1.48, screen_size[1]/2.1))
-
-highlight_colors ={
-    "red": (255, 0, 0)
-    "green": (0,255,0)
-}
-
-while True:
-for event in pygame.event.get():
-    if event.type == pygame.:
-        font = pygame.font.Font(None, 48)
-        text = font.render("Correct!", True, "black")
-        window.blit(text, (half_of_width, half_of_height))
-    elif event.type =/= pygame.:
-        font = pygame.font.Font(None, 48)
-        text = font.render("Correct!", True, "black")
-        window.blit(text, (half_of_width, half_of_height))
-pygame.display.flip()
-pygame.time.wait(2000)
 
