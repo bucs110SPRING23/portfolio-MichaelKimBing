a8df0e8b7fe7df6ad97e8392331dc17cf6bdef98
Michael Kim <gamingleaf01@gmail.com>
2023-03-06 17:22:12 -0500

    Working on Exercise Coin Flip

--- ch04/exercises/coinflip.py
+++ ch04/exercises/coinflip.py
@@ -5,9 +5,27 @@ window = turtle.Screen()
 my_turtle = turtle.Turtle()
 my_turtle.shape("turtle")
 my_turtle.color("blue")
-heads = my_turtle.left(90)
-tails = my_turtle.right(90)
-coin_flip_forward = my_turtle.forward(50)
+my_turtle.speed(0)
+distance = 10
+angle = 0 
+is_in_screen = True
 
+flip = []
+while is_in_screen:
+    coin = random.randint(0,1)
+    if coin:
+        my_turtle.right(angle)
+    else:
+        my_turtle.left(angle)
+    my_turtle.forward(distance)
 
-window.exitonclick()
+    turtleX = my_turtle.xcor()
+    turtleY = my_turtle.ycor()
+
+    x_range = window.window_width()/2
+    y_range = window.window_height()/2
+
+    if abs(turtleX) > x_range or abs(turtleY) > y_range:
+        is_in_screen = False
+
+# window.exitonclick()
