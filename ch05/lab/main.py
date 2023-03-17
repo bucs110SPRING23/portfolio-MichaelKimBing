7cd689dd912a5ccd0fbb353eb15d20e3c24daf5d
Michael Kim <gamingleaf01@gmail.com>
2023-03-17 00:42:14 -0400

    Working on Chapter 5 Lab

--- ch05/lab/main.py
+++ ch05/lab/main.py
@@ -3,7 +3,7 @@
 
 #Part A
 def threenp1(n):
-    count =  0
+    count = 0
     while n > 1.0:
         count +=1
         if n % 2 == 0:
@@ -12,19 +12,52 @@ def threenp1(n):
             n = int(3 * n + 1)
     return count
 
+
 def threenp1range(upper_limit):
-    n = int(input("Enter A Number:"))
-    iters = threenp1(n)
-    objs_in_sequence = {n:iters}
+    objs_in_sequence = {i:threenp1(n)} #suggestion
+    if upper_limit < 1:
+        return {}
+    threenp1()
+    objs_in_sequence.append(upper_limit)
     return objs_in_sequence
 
 def main():
-    for i in range(3, upper_limit):
-        threenp1()
-        threenplus1_iters_dictionary["n"] = "iterations"
-    return upper_limit
-
+    upper_limit = int(input("Please Enter a Limit Number:"))
+    for i in range(3,upper_limit):
+        threenp1(n)
+        threenp1range(n)
+        return main
+    
 main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def main():
+#     for i in range(3, upper_limit):
+#         threenp1()
+#         threenplus1_iters_dictionary["n"] = "iterations"
+#     return upper_limit
+
+# main()
 
 # if __name__ == "__main__":
 # main()
