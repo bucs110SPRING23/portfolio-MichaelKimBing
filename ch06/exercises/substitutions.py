8fcfba027751399e7ba90ce8975389ad9d0b69d2
Michael Kim <gamingleaf01@gmail.com>
2023-03-20 21:42:10 -0400

    Completed Exercise: Substituions

--- ch06/exercises/substitutions.py
+++ ch06/exercises/substitutions.py
@@ -1,11 +1,17 @@
 import json
 
 def main():
-    fptr = open("news.txt","a")
-    ideas = fptr.read()
-    fptr.readlines()
-    change_words = {
-        "tokens":"bananas",
-        "enthusiasts":"fools",
-    }
-    fptr.close()
+    file_pointer = open("news.txt","r")
+    reading_txt = file_pointer.read()
+    file_pointer.close()
+    fptr = open("editing.json", "r")
+    ideas = json.load(fptr)
+    for k, v, in ideas.items():
+        reading_txt = reading_txt.replace(k,v)
+        print(k,v)
+
+    txt_new = open("better_news.txt","w")
+    txt_new.write(reading_txt)
+    txt_new.close()
+
+main()
