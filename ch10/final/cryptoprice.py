3b5b83ff3c7c981a0cc21a8d61e086a3aab7bbcf
Michael Kim <gamingleaf01@gmail.com>
2023-05-07 00:00:26 -0400

    Completed Final Exam

--- ch10/final/cryptoprice.py
+++ ch10/final/cryptoprice.py
@@ -2,11 +2,13 @@ import requests
 
 class Cryptoprice:
 
-    def __init__(self,skip = 0,limit = 1):
-        self.api_url = f"https://api.coinstats.app/public/v1/coins?skip={skip}&limit={limit}"
+    def __init__(self):
+        self.api_url = f"https://api.coinstats.app/public/v1/coins"
 
     def get(self):
-        r = requests.get(self.api_url)
+        self.skip = 0
+        self.limit = 1
+        r = requests.get(f'{self.api_url}?{self.skip}&{self.limit}')
         response = r.json()
         bitcoin_dict = response
         return bitcoin_dict['coins'][0]['name'],bitcoin_dict['coins'][0]['price']
